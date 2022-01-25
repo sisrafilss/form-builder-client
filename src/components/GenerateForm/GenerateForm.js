b3639798370971f5e1c719f4a4273f75c38a7a52
Israfil Hossen <sisrafilss@gmail.com>
2022-01-25 12:22:11 +0600

    route, navigation added

---
+++ src/components/GenerateForm/GenerateForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+const GenerateForm = () => {
+    return (
+        <div>
+            <Navigation />
+            <h2>This is Generate Form</h2>
+        </div>
+    );
+};
+
+export default GenerateForm;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+
+const Home = () => {
+  return (
+    <>
+      <Navigation />
+
+      <div className="container mt-5">
+        <div className="card shadow">
+          <div className="card-header">Form List</div>
+          <div className="card-body"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
