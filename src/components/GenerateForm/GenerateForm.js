825ab6ece7d8bd45d5fa7b496be49d7eb06c7a18
Israfil Hossen <sisrafilss@gmail.com>
2022-01-25 15:05:01 +0600

    form generate page added

--- src/components/GenerateForm/GenerateForm.js
+++ src/components/GenerateForm/GenerateForm.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import Navigation from '../Navigation/Navigation';
+import React from "react";
+import Navigation from "../Navigation/Navigation";
 
 const GenerateForm = () => {
-    return (
-        <div>
-            <Navigation />
-            <h2>This is Generate Form</h2>
+  return (
+    <>
+      <Navigation />
+      <div className="container mt-5">
+        <div className="d-flex justify-content-between">
+          <div>
+            <form>
+              <div>
+                <label htmlFor="formName" className="mb-2">
+                  Form Name
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+            </form>
+          </div>
+          <div>
+            <button className="btn btn-success d-block mb-2">Text</button>
+            <button className="btn btn-success d-block mb-2">Number</button>
+            <button className="btn btn-success d-block mb-2">Date</button>
+            <button className="btn btn-success d-block mb-2">Text Area</button>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default GenerateForm;
